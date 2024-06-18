1fb041b570fbfdd8a986374fbe61a25a0009dff4
gitlayzer <gitlayzer@devops-engineer.com.cn>
2024-06-18 18:44:32 +0800

    Update WebSite ~~~

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,27 +1,27 @@
 <script setup>
 import WelcomeItem from '@/components/WelcomeItem.vue'
-import DocumentationIcon from '@/components/icons/IconDocumentation.vue'
-import ToolingIcon from '@/components/icons/IconTooling.vue'
-import EcosystemIcon from '@/components/icons/IconEcosystem.vue'
-import CommunityIcon from '@/components/icons/IconCommunity.vue'
-import SupportIcon from '@/components/icons/IconSupport.vue'
+import DockerIcon from '@/components/icons/IconDocker.vue'
+import QuayIcon from '@/components/icons/IconQuay.vue'
+import KubernetesIcon from '@/components/icons/IconKubernetes.vue'
+import GithubIcon from '@/components/icons/IconGithub.vue'
+import GoogleIcon from '@/components/icons/IconGoogle.vue'
+import BlogIcon from '@/components/icons/IconBlog.vue'
 </script>
 
 <template>
   <WelcomeItem>
     <template #icon>
-      <DocumentationIcon />
+      <DockerIcon />
     </template>
     <template #heading>Docker</template>
-    <h4>官方方法：docker pull nginx:alpine 或 docker pull bitnami/kubectl</h4>
+    <h4>官方方法：docker pull nginx:alpine</h4>
     <h4>加速方法：docker pull docker.cloudimages.asia/nginx:alpine</h4>
-    <h4>加速方法：docker pull docker.cloudimages.asia/bitnami/kubectl</h4>
     <br />
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
-      <EcosystemIcon />
+      <KubernetesIcon />
     </template>
     <template #heading>Kubernetes</template>
     <h4>官方方法：docker pull registry.k8s.io/coredns/coredns:v1.11.1</h4>
@@ -31,17 +31,7 @@ import SupportIcon from '@/components/icons/IconSupport.vue'
 
   <WelcomeItem>
     <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>GCR</template>
-    <h4>官方方法：docker pull gcr.io/username/image:tag</h4>
-    <h4>加速方法：docker pull gcr.cloudimages.asia/username/image:tag</h4>
-    <br />
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
+      <QuayIcon />
     </template>
     <template #heading>Quay</template>
     <h4>官方方法：docker pull quay.io/username/image:tag</h4>
@@ -51,10 +41,30 @@ import SupportIcon from '@/components/icons/IconSupport.vue'
 
   <WelcomeItem>
     <template #icon>
-      <SupportIcon />
+      <GithubIcon />
     </template>
     <template #heading>GHCR</template>
     <h4>官方方法：docker pull ghcr.io/username/image:tag</h4>
     <h4>加速方法：docker pull ghcr.cloudimages.asia/username/image:tag</h4>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <GoogleIcon />
+    </template>
+    <template #heading>GCR</template>
+    <h4>官方方法：docker pull gcr.io/username/image:tag</h4>
+    <h4>加速方法：docker pull gcr.cloudimages.asia/username/image:tag</h4>
+    <br />
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <BlogIcon />
+    </template>
+    <template #heading>BLOG</template>
+    <h4>第一域名：https://blog.devops-engineer.com.cn</h4>
+    <h4>第二域名：https://www.devops-engineer.com.cn</h4>
+    <br />
   </WelcomeItem>
 </template>
